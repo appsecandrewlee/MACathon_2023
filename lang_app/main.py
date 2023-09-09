b19c525274461a0ad9bee07dcb10fba24d1fd950
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-09-09 17:38:34 +1000

    segsegsegseg
    xdgsgsg

--- lang_app/main.py
+++ lang_app/main.py
@@ -13,6 +13,40 @@ db = firestore.client()
 
 # Initialize Google Translate
 translate_client = translate.Client()
+def translate_text(text="Hello, world!", project_id="macathon2023-27687"):
+
+    client = translate.TranslationServiceClient()
+    location = "global"
+    parent = f"projects/{project_id}/locations/{location}"
+
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",
+            "source_language_code": "en-US",
+            "target_language_code": "es",
+        }
+    )
+
+    for translation in response.translations:
+        print("Translated text: {}".format(translation.translated_text))
+
+
+def detect_language(text: str) -> dict:
+    """Detects the text's language."""
+    translate_client = translate.Client()
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.detect_language(text)
+
+    print(f"Text: {text}")
+    print("Confidence: {}".format(result["confidence"]))
+    print("Language: {}".format(result["language"]))
+
+    return result
+
 
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
--- langtrans_app/main.py
+++ langtrans_app/main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import numpy as np
-from google.cloud import translate_v2 as translate
+from google.cloud import translate as translate
+from google.cloud import texttospeech
+# from google.cloud import vision
 from firebase_admin import credentials, firestore, initialize_app
 from firebase_admin import auth
 
 app = FastAPI()
 
-# Initialize Firebase Admin
-cred = credentials.Certificate("/Users/vellichastrxism/Desktop/Macathon/MACathon_2023/langtrans_app/macathon2023-27687-firebase-adminsdk-t5le9-d7a6865b5b.json")
-initialize_app(cred)
-db = firestore.client()
+# # Initialize Firebase Admin
+# cred = credentials.Certificate("/Users/vellichastrxism/Desktop/Macathon/MACathon_2023/langtrans_app/macathon2023-27687-firebase-adminsdk-t5le9-d7a6865b5b.json")
+# initialize_app(cred)
+# db = firestore.client()
 
 # Initialize Google Translate
 # translate_client = translate.Client()
@@ -43,8 +45,6 @@ def verify_token(id_token: str):
 
 
 @app.post("/upload/")
-
-
 async def upload_image(file: UploadFile = File(...)):
     image_stream = await file.read()
     image_np = np.fromstring(image_stream, np.uint8)
@@ -64,3 +64,56 @@ async def upload_image(file: UploadFile = File(...)):
     })
 
     return {"original": text, "translated": translated_text}
+
+
+
+# Initialize Google Translate
+def translate_text(
+    text: str = "YOUR_TEXT_TO_TRANSLATE", project_id: str = "macathon2023-27687"
+) -> translate.TranslationServiceClient:
+    """Translating Text."""
+
+    client = translate.TranslationServiceClient()
+
+    location = "global"
+
+    parent = f"projects/{project_id}/locations/{location}"
+
+    # Translate text from English to French
+    # Detail on supported types can be found here:
+    # https://cloud.google.com/translate/docs/supported-formats
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",  # mime types: text/plain, text/html
+            "source_language_code": "en-US",
+            "target_language_code": "fr",
+        }
+    )
+
+    # Display the translation for each input text provided
+    for translation in response.translations:
+        print(f"Translated text: {translation.translated_text}")
+
+    return response
+
+
+def detect_language(text: str) -> dict:
+    """Detects the text's language."""
+    from google.cloud import translate_v2 as translate
+    translate_client = translate.Client()
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.detect_language(text)
+
+    print(f"Text: {text}")
+    print("Confidence: {}".format(result["confidence"]))
+    print("Language: {}".format(result["language"]))
+
+    return result
+
+
+print(translate_text("My name is Johnny"))
+print(detect_language("Halo nama saya"))
