61740f2fc56cbbae5c0f1041316e071a6362717f
williamdoto <87979482+williamdoto@users.noreply.github.com>
2023-09-09 15:53:07 +1000

    new branch

--- langtrans_app/App.js
+++ langtrans_app/App.js
@@ -43,6 +43,8 @@ export default function App() {
       console.error("Error verifying token:", error);
     }
   };
+
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
--- langtrans_app/services/api.js
+++ langtrans_app/services/api.js
@@ -23,3 +23,38 @@ export const sendImageToServer = async (imageUri) => {
     console.error("Error uploading the image:", error);
   }
 };
+
+export const handleSignUp = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000", {
+        email: form.email,
+        password: form.password,
+      });
+
+      if (response.data && response.data.uid) {
+        Alert.alert("Success!", "Account created successfully.");
+      } else {
+        Alert.alert("Error", "Failed to create account.");
+      }
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }
+  };
+
+ export const verifyUserToken = async (token) => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/verify-token/", {
+        id_token: token,
+      });
+
+      if (response.data && response.data.uid) {
+        console.log("Verified UID:", response.data.uid);
+        // Handle successful verification
+      } else {
+        console.log("Token verification failed.");
+        // Handle failed verification
+      }
+    } catch (error) {
+      console.error("Error verifying token:", error);
+    }
+  };
